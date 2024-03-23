2766a5342c02a4c1ee09502196ff9c754bc77227
Irakli Orjonikidze <96581851+IrakliOrjo@users.noreply.github.com>
2024-03-23 17:02:37 +0400

    Update Journal.jsx

---
+++ src/components/Journal.jsx
@@ -0,0 +1,20 @@
+ 
+import image1 from '../assets/2.jpg'
+import image2 from '../assets/3.jpg'
+import image3 from '../assets/4.jpg'
+ 
+
+const journalData = [
+    {
+        text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ullam expedita ex eos qui autem`,
+        img: image1
+    },
+    {
+        text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ullam expedita ex eos qui autem`,
+        img: image2
+    },
+    {
+        text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ullam expedita ex eos qui autem`,
+        img: image3
+    },
+]
