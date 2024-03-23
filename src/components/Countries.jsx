aaebb00b9144518634dad42e59b1e20003c2f0ef
Irakli Orjonikidze <96581851+IrakliOrjo@users.noreply.github.com>
2024-03-23 17:18:46 +0400

    Update Countries.jsx

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
 
+const countriesData = [
+    {
+        title: "Southern countries",
+        description: "Lorem ipsum dolor amet, consecte tempor incididunt ut labore et dolore tumber tur adipisicing elit."
+    },
+    {
+        title: "Nordic countries",
+        description: "Lorem ipsum dolor amet, consecte tempor incididunt ut labore et dolore tumber tur adipisicing elit."
+    },
+    {
+        title: "African countries",
+        description: "Lorem ipsum dolor amet, consecte tempor incididunt ut labore et dolore tumber tur adipisicing elit."
+    },
+
+]
+
 const Countries = () => {
   return (
     <div>Countries</div>
